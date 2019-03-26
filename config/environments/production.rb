ec02398809e1720d8776ce4cdb7e3976f0d6508e
Stephen Lind <stephen.l@getjobber.com>
2019-03-26 14:51:46 -0600

    update order of junk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,13 @@ Rails.application.configure do
   host = 'hometown-40k.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :address        => 'smtp.sendgrid.net',
+    :port           => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
 end
