5d4ff3668c4533f5bfc7f0a7984234f55aac2482
Stephen Lind <stephen.l@getjobber.com>
2019-04-28 20:25:08 -0600

    update for searchbox

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,5 @@
 if File.exists?("config/elasticsearch.yml")
   config = YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys
+  config[:host] = ENV['SEARCHBOX_URL']
   Elasticsearch::Model.client = Elasticsearch::Client.new(config)
 end
